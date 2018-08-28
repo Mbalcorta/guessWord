d818f26f8b2e4887a70cf65ebf459a5c4301df53
Mateo Balcorta <misterrattail@gmail.com>
2018-08-28 12:00:44 -0700

    Add onclick events to buttons when user either loses or wins.

--- src/Components/PlayAgainButton/PlayAgainButton.js
+++ src/Components/PlayAgainButton/PlayAgainButton.js
@@ -2,15 +2,26 @@ import React from 'react';
 import PropTypes from "prop-types";
 
 const PlayAgainButton = (props) => {
-  return(
-    <div>
-      <button>{props.text}</button>
-    </div>
-  );
+  const {restartGame, text, levelUp } = props; 
+  if(restartGame){
+    return(
+      <div>
+        <button onClick={restartGame}>{text}</button>
+      </div>
+    );
+  } else {
+    return(
+      <div>
+        <button onClick={levelUp}>{text}</button>
+      </div>
+    );
+  }
+  
 };
 
 PlayAgainButton.propTypes = {
-  text: PropTypes.string
+  text: PropTypes.string,
+  restartGame: PropTypes.func
 };
 
 export default PlayAgainButton; 
